9be205458993661da48d89f19985a995e51ea49e
Rodrigo G. Oliveira <mjgargani@gmail.com>
2023-09-03 01:10:59 -0300

    Proj01; Adc Post 1.5/2 feedback area

--- ignite/reactjs/fundamentals/proj01/src/components/Post.jsx
+++ ignite/reactjs/fundamentals/proj01/src/components/Post.jsx
@@ -13,11 +13,20 @@ export function Post(){
         </div>
         <time title="11 de Maio às 08:13h" dateTime='2022-05-11 08:13:30'>Publicado há 1h</time>
       </header>
+
       <div className={styles.content}>
         <p>Fala galera!</p>
         <p>Esse é meu post!</p>
         <p><a href="#">http://gargani.dev</a><a href="#">http://gargani.dev</a></p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder='Deixe um comentário'/>
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+      </form>
     </article>
   )
 }
